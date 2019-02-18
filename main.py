387dac2d90682cde8b81e89360e116ead64de0e1
John Liu <33496334+jliu01298@users.noreply.github.com>
2019-02-17 22:47:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
 from display import *
 from draw import *
 from matrix import *
-import random
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
 matrix = new_matrix()
+_matrix=[]
 
-
-'''add_edge(matrix,150,0,0,499,0,0)
-add_edge(matrix,499,0,0,250,499,0)
-add_edge(matrix,250,499,0,150,0,0)'''
-w=0
-x=0
-y=0
-z=0
-for i in range(25):
-    y=(y+176)%500
-    z=(z+368)%500
-    add_edge(matrix,w,x,0,y,z,0)
-    w,x=y,z
-else:
-    add_edge(matrix,w,x,0,0,0,0)
+add_edge(matrix,0,0,0,25,0,0)
+add_edge(matrix,25,0,0,25,25,0)
+add_edge(matrix,25,25,0,0,25,0)
+add_edge(matrix,0,25,0,0,0,0)
+add_edge(_matrix,6,4,0,8,13,0)
+add_edge(_matrix,14,13,0,25,6,0)
+add_edge(_matrix,23,21,0,29,25,0)
+add_edge(_matrix,11,15,0,13,17,0)
+draw_lines( matrix, screen, color )
+matrix_mult(_matrix,matrix)
 draw_lines( matrix, screen, color )
 display(screen)
+
